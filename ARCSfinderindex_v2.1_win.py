18f643df4bae8b719e8d273730ad699bb197beeb
fparia17 <parisfilippou@gmail.com>
2018-10-12 21:09:18 +0300

    Add files via upload

--- ARCSfinderindex_v2.1_win.py
+++ ARCSfinderindex_v2.1_win.py
@@ -1,55 +1,43 @@
 import os, shutil
-from PermitImport import PermitImport
 
-#Import ARCS CD directory path and chart ID
-ARCSDIR = input("Insert ARCS CD directory:")
-#ChartID = input("Type ARCS chart ID:")
+while True:
+    #Import ARCS CD directory path and chart ID
+    ARCSDIR = input("Insert ARCS CD directory:")
+    ChartID = input("Type ARCS chart ID:")
 
-#Import 
-ChartID = PermitImport()
-
-#Create new list for three digit code. Parse the ARCS IDs to add a 0. 
-ThreeDID = []
-for i in ChartID:
-    dig = '0' + i[0:2]
-    ThreeDID.append(dig)
-
-#Parse directory to move outside it
-ARCSDIRNew = ARCSDIR[:-23]
-os.chdir(ARCSDIRNew)
-
-#Create the data folder and copy standard content within it.
-shutil.copytree(ARCSDIR, ARCSDIRNew+'/ARCS')
-
-#Remove the RASCHTS directory with all charts data
-shutil.rmtree(ARCSDIRNew+'/ARCS/RASCHTS')
-
-#Create a new RASCHTS file with subfolder and copy chart's data
-for i in ThreeDID:
-    shutil.copytree(ARCSDIR+'/RASCHTS/'+ThreeDID[i], ARCSDIRNew+'/ARCS/RASCHTS/'+ThreeDID[i])
-
+    print('"ARCS_Data" folder created!')
+    print("Coping chart's data to folder...")
     
-'''
-#Parse to extract the first 3 digits of the ARCS ID
-ChartID = str(ChartID)
-ThreeDigID = '0' + ChartID[0:2]
+    #Parse to extract the first 3 digits of the ARCS ID
+    ChartID = str(ChartID)
+    ThreeDigID = '0' + ChartID[0:2]
 
-#Parse directory to move outside it
-ARCSDIRNew = ARCSDIR[:-23]
-os.chdir(ARCSDIRNew)
-
-#Copy standard content
-shutil.copytree(ARCSDIR, ARCSDIRNew+'/ARCS')
-
-#Remove the RASCHTS directory with all charts data
-shutil.rmtree(ARCSDIRNew+'/ARCS/RASCHTS')
-
-#Create a new RASCHTS file with subfolder and copy chart's data 
-shutil.copytree(ARCSDIR+'/RASCHTS/'+ThreeDigID+'/'+ChartID, ARCSDIRNew+'/ARCS/RASCHTS/'+ThreeDigID+'/'+ChartID)
+    #Parse directory to move outside it
+    ARCSDIRNew = ARCSDIR[:-23]
+    os.chdir(ARCSDIRNew)
+    
+    if os.path.exists(ARCSDIRNew+'/ARCS'):
+        print('''Adding chart's data to "ARCS_Data" folder...''')
+        shutil.copytree(ARCSDIR+'/RASCHTS/'+ThreeDigID+'/'+ChartID, ARCSDIRNew+'/ARCS/RASCHTS/'+ThreeDigID+'/'+ChartID)
+    else:
+        #Copy standard content
+        shutil.copytree(ARCSDIR, ARCSDIRNew+'/ARCS')
+        #Remove the RASCHTS directory with all charts data
+        shutil.rmtree(ARCSDIRNew+'/ARCS/RASCHTS')
+        #Create a new RASCHTS file with subfolder and copy chart's data 
+        shutil.copytree(ARCSDIR+'/RASCHTS/'+ThreeDigID+'/'+ChartID, ARCSDIRNew+'/ARCS/RASCHTS/'+ThreeDigID+'/'+ChartID)
+    
+    choice = input("\nAdd another chart? y/n:")
+    
+    if choice == 'y':
+        continue
+    else:
+        break
 
 #Create archive .zip
 os.chdir(ARCSDIRNew)
-shutil.make_archive('ARCS_'+ChartID, 'zip', 'ARCS')
+shutil.make_archive('ARCS_Data', 'zip', 'ARCS')
 
-print('Done!!!')
-'''
+print('\n==========================================\n')
+print('Done!!!\nCompressed file ready.')
+print('"ARCS_Data.zip" available at, path:'+ARCSDIRNew)
